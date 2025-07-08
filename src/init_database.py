c14f74010fb6c0acac711277fbfa63b9318d22e6
alexsander junior <arcarvalhojr@gmail.com>
2025-07-08 20:45:38 -0300

    fix database path

--- src/init_database.py
+++ src/init_database.py
@@ -1,9 +1,14 @@
 # %%
+from pathlib import Path
 import duckdb
 
+# Define the path for the database
+base_dir = Path.cwd().parent
+db_path = base_dir / "sih_sus.duckdb"
 
+# %%
 # Create a DuckDB data base
-con = duckdb.connect("../../sih_sus.duckdb")
+con = duckdb.connect(str(db_path))
 
 # Create schemas
 con.execute("CREATE SCHEMA IF NOT  EXISTS bronze")
