d2b399fd175a47ff98183b27ff3cafeb720480b1
alexsander junior <arcarvalhojr@gmail.com>
2025-07-08 22:46:21 -0300

    change tables on bronze

--- src/bronze/injest_bronze.py
+++ src/bronze/injest_bronze.py
@@ -1,48 +1,18 @@
 # %%
 import duckdb
-import os
 
 # Open the connection with the data base
 con = duckdb.connect("../../sih_sus.duckdb")
 con.execute("SET SCHEMA 'bronze'")
 
 # %%
-# Load sih sus data into the bronze schema
-sih_path = os.path.join("../../data/sih", "**", "*.parquet").replace("\\", "/")
-
-sih_columns = [
-    'ANO_CMPT', 'UF_ZI', 'DIAS_PERM', 'SEXO',
-    'IDADE', 'DIAG_PRINC', 'VAL_SH', 'VAL_TOT', 'US_TOT'
-]
-columns_str = ", ".join(sih_columns)
-
-con.execute(f"""
-    CREATE OR REPLACE TABLE bronze.sih_sus AS
-    SELECT {columns_str}
-    FROM read_parquet('{sih_path}', union_by_name= True)
-""")
-
-# %%
-sih_sus_result = con.sql(f'''
-    SELECT 
-        ANO_CMPT,
-        COUNT (*) AS Total_registros,
-        SUM(VAL_TOT) AS Total_gastos
-    FROM sih_sus
-    GROUP BY ANO_CMPT
-    ORDER BY ANO_CMPT
-''').df()
-sih_sus_result
-
-# %%
 # Load the localidade file into the bronze schema
-# Convert codigo_ibge_cidade and codigo_uf to varchar
 con.execute(f"""
     CREATE OR REPLACE TABLE bronze.localidade AS
     SELECT
-        CAST(codigo_ibge_cidade AS VARCHAR) AS codigo_ibge_cidade,
+        CAST(codigo_ibge_cidade AS INTEGER) AS codigo_ibge_cidade,
         nome_cidade,
-        CAST(codigo_uf AS VARCHAR) AS codigo_uf,
+        CAST(codigo_uf AS INTEGER) AS codigo_uf,
         sigla_uf,
         nome_estado
     FROM read_parquet('../../data/localidade.parquet')
@@ -55,14 +25,15 @@ localidade_result
 # Load the cid10_icsap file into the bronze schema
 con.execute(f"""
     CREATE OR REPLACE TABLE bronze.cid10_icsap AS
-    SELECT * FROM read_csv_auto('../../data/cid10_icsap.csv', header= True)
+    SELECT * 
+    FROM read_csv_auto('../../data/cid10_icsap.csv', header= True)
 """)
 
 cid10_icsap_result = con.sql('SELECT * FROM cid10_icsap LIMIT 10').df()
 cid10_icsap_result
 
 # %%
-# Last check if all tables were created
+# Check if all tables were created
 con.sql("""
     SELECT table_schema, table_name
     FROM information_schema.tables
