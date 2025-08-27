b55cbbf7917751026d17db519a41fc7ab0088363
alexsander junior <arcarvalhojr@gmail.com>
2025-08-26 23:01:26 -0300

    change for disease category profile

---
+++ src/03_gold/sih_2024_icsap_category_gold.sql
@@ -0,0 +1,24 @@
+/*
+2024 DISEASE CATEGORY PROFILE
+Calculated metrics:
+- Total hospitalizations and associated costs per avoidable disease category  
+- Average cost and average length of stay per avoidable disease category
+*/
+
+SELECT
+      cid.disease_category,
+
+      COUNT(*) AS total_hospitalizations,
+      ROUND(SUM(sih.total_paid), 2) AS total_cost,
+
+      ROUND(AVG(sih.total_paid), 2) AS avg_cost,
+      ROUND(AVG(sih.length_stay), 2) AS avg_length_stay,
+
+FROM silver.sih_sus AS sih
+LEFT JOIN silver.cid10_icsap AS cid
+      ON sih.disease_code = cid.avoidable_disease_code
+WHERE sih.year = 2024
+      AND cid.avoidable_disease_code IS NOT NULL
+      AND sih.total_paid > 0
+GROUP BY cid.disease_category
+
