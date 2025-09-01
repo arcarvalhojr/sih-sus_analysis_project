5764b897ca4695c150a665133da777e7190fa8f8
alexsander junior <arcarvalhojr@gmail.com>
2025-09-01 19:50:59 -0300

    add 2014 data

--- src/03_gold/sih_2024_icsap_category_gold.sql
+++ src/03_gold/sih_2024_icsap_category_gold.sql
@@ -6,19 +6,20 @@ Calculated metrics:
 */
 
 SELECT
+      sih.year,
       cid.disease_category,
 
       COUNT(*) AS total_hospitalizations,
       ROUND(SUM(sih.total_paid), 2) AS total_cost,
 
       ROUND(AVG(sih.total_paid), 2) AS avg_cost,
-      ROUND(AVG(sih.length_stay), 2) AS avg_length_stay,
+      ROUND(AVG(sih.length_stay), 2) AS avg_length_stay
 
 FROM silver.sih_sus AS sih
 LEFT JOIN silver.cid10_icsap AS cid
       ON sih.disease_code = cid.avoidable_disease_code
-WHERE sih.year = 2024
+WHERE sih.year IN (2014, 2024)
       AND cid.avoidable_disease_code IS NOT NULL
       AND sih.total_paid > 0
-GROUP BY cid.disease_category
+GROUP BY sih.year, cid.disease_category
 
--- src/03_gold/sih_2024_regional_icsap_gold.sql
+++ src/03_gold/sih_2024_regional_icsap_gold.sql
@@ -5,21 +5,22 @@ Calculated metrics:
 - Average cost per hospitalization across geographic and clinical dimensions
 */
 
-SELECT 
+SELECT
+      sih.year,
       uf.big_region_name,
       uf.state_name,
       cid.disease_category,
 
       COUNT(*) AS total_hospitalizations,
       SUM(sih.total_paid) AS total_cost,
-      ROUND(AVG(sih.total_paid), 2) AS avg_total_cost,
+      ROUND(AVG(sih.total_paid), 2) AS avg_total_cost
 
 FROM silver.sih_sus AS sih
 LEFT JOIN silver.cid10_icsap AS cid
       ON sih.disease_code = cid.avoidable_disease_code
 LEFT JOIN silver.uf_localidade AS uf
       ON sih.state_code = uf.state_code
-WHERE sih.year = 2024
+WHERE sih.year IN (2014, 2024)
       AND cid.avoidable_disease_code IS NOT NULL
       AND sih.total_paid > 0
-GROUP BY ALL
+GROUP BY sih.year, uf.big_region_name, uf.state_name, cid.disease_category
